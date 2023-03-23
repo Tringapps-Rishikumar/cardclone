da077a4703c8c21f3eaa4709f86a6748db8ebc2b
Tringapps-Rishikumar <rishikumar.d@tringapps.com>
2023-03-23 11:11:49 +0530

    Initial commit

---
+++ src/main/java/card/Card.java
@@ -0,0 +1,42 @@
+package card;
+
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+public class  Card implements Cloneable {
+    public Logger l =  Logger.getLogger("points");
+    public String holdername;
+    public String cardnumber;
+    public String expirationdate;
+    String name = "aa";
+    String number = "1248";
+    String date = "12";
+
+    public Card() {
+        l.info("\n enter the Holders name,Card Number and Expiration Date :");
+
+        Scanner sc = new Scanner(System.in);
+        holdername = sc.next();
+        cardnumber = sc.next();
+        expirationdate = sc.next();
+        l.info("the original card");
+        l.log(Level.INFO ,()-> "\n Holders name "+ name);
+        l.log(Level.INFO,()->"\n  Card Number "+number);
+        l.log(Level.INFO,()->"\n Expiration Date "+date);
+    }
+    public String cardNumber() {
+        if (cardnumber.equals(number)) {
+            return "true";
+        } else {
+            return "false";
+        }
+    }
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        try {
+            return super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new CloneNotSupportedException();
+        }
+    }
+}
---
+++ src/main/java/org/example/Cardmain.java
@@ -0,0 +1,25 @@
+package org.example;
+import card.Card;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+class Cardmain{
+    public static void main(String[]args)throws CloneNotSupportedException{
+        Card card1=new Card();
+        Logger l =  Logger.getLogger("points");
+        if(card1.cardNumber().equals("true"))
+        {
+        l.info("card with same number");
+        }
+        else{
+        l.info("the card with diferent number");
+        }
+        Card card2 = (Card) card1.clone();
+        l.info("the cloned card:");
+        l.log(Level.INFO ,()-> "\n Holders name "+ card2.holdername);
+        l.log(Level.INFO,()->"\n  Card Number "+card2.cardnumber);
+        l.log(Level.INFO,()->"\n Expiration Date "+card2.expirationdate);
+
+
+    }
+}
+
